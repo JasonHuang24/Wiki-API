36e16be5f608a608bef79e74eef6551d3046987a
Jason <33882681+JasonHuang24@users.noreply.github.com>
2023-01-29 20:43:39 -0700

    Commit 3 -Lesson 367 DELETE All Articles

--- app.js
+++ app.js
@@ -35,6 +35,32 @@ app.get("/articles", function(req, res){
   });
 });
 
+app.post("/articles", function(req, res){
+
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content
+  });
+
+  newArticle.save(function(err){
+    if(!err){
+      res.send("Successfully added a new article.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.delete("/articles", function(req, res){
+  Article.deleteMany(function(err){
+    if(!err){
+      res.send("Successfully deleted all articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
